ef30773b8d42ff72727b6e5f8a76d70cd372924a
J David P <jesayadavidgnp@gmail.com>
2025-02-06 23:26:45 +0700

    Button Improv + Slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Header from './Comps/Header';
+import { TracingBeam } from './components/ui/tracing-beam';
 
 function App() {
   const [] = useState(0);
@@ -8,9 +9,20 @@ function App() {
   return (
     <>
       <Header />
-      <div>
-        {/* Other content */}
-      </div>
+      <TracingBeam className="
+        -ml-30             /* mobile (default: <640px) */
+        sm:-ml-40         /* sm: 640px (~23% of 2xl) */
+        md:-ml-65         /* md: 768px (~40% of 2xl) */
+        lg:-ml-75    /* lg: 1024px (~60% of 2xl) */
+        xl:-ml-90    /* xl: 1280px (~83% of 2xl) */
+        2xl:-ml-110       /* 2xl: 1536px (current) */
+        mt-10
+        -z-10
+      ">
+        <div className="h-[600vh]">
+          {/* Other content */}
+        </div>
+      </TracingBeam>
     </>
   );
 }
